b599cd2d60e4736e7ebff5f53d81ff8538239c1e
AriZhong <rest950@gmail.com>
2021-03-24 16:01:32 +0800

    20210324_start

--- android/app/src/main/kotlin/com/arizhong/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/arizhong/flutter_app/MainActivity.kt
@@ -1,11 +1,4 @@
 package com.arizhong.flutter_app
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity : FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
+class MainActivity : FlutterActivity() {
 }
--- lib/pages/individualphoto.dart
+++ lib/pages/individualphoto.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/model/photo.dart';
 import 'package:flutter_app/service/userrepository.dart';
 import 'package:http/http.dart' as http;
-import 'package:local_auth/local_auth.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:share/share.dart';
@@ -16,9 +15,7 @@ class IndividualPhotoPage extends StatefulWidget {
 
 class _IndividualPhotoPageState extends State<IndividualPhotoPage>
     with SingleTickerProviderStateMixin {
-  final auth = LocalAuthentication();
   double _scaleRadio = 0.8;
-  bool selected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +31,7 @@ class _IndividualPhotoPageState extends State<IndividualPhotoPage>
             icon: Icon(Icons.share),
             onPressed: () async {
               //TODO: 使用生物辨識
-              try {
-                if (await auth.canCheckBiometrics) {
-                  if (await auth.authenticate(
-                      localizedReason: 'Please authenticate to share photo.',
-                      biometricOnly: false)) {
-                    await shareImage(photo);
-                  }
-                }
-              } catch (e) {}
+              try {} catch (e) {}
             },
           )
         ],
@@ -55,62 +44,8 @@ class _IndividualPhotoPageState extends State<IndividualPhotoPage>
             leading: Text('Id: ${photo.id}'),
           ),
           //TODO: AnimatedOpacity, AnimatedPadding, AnimatedContainer
-          AnimatedOpacity(
-            opacity: selected ? 0 : 1,
-            duration: Duration(milliseconds: 1500),
-            curve: Curves.linear,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.network(
-                photo.url,
-              ),
-            ),
-          ),
           // //TODO: AnimatedSwitcher
-          // AnimatedSwitcher(
-          //   duration: Duration(milliseconds: 1000),
-          //   child: selected
-          //       ? Container(
-          //           key: ValueKey(1),
-          //           color: Colors.white,
-          //           height: 100,
-          //           width: 50,
-          //         )
-          //       : FlutterLogo(
-          //           size: 20,
-          //         ),
-          //   switchInCurve: Curves.easeIn,
-          //   switchOutCurve: Curves.easeIn,
-          // ),
           // //TODO: TweenAnimationBuilder
-          // TweenAnimationBuilder<double>(
-          //     tween: Tween<double>(begin: 0, end: 2 * math.pi),
-          //     duration: Duration(seconds: 2),
-          //     curve: Curves.easeIn,
-          //     builder: (__, angle, child) {
-          //       return Transform.rotate(
-          //         angle: angle,
-          //         child: FlutterLogo(
-          //           size: 100,
-          //         ),
-          //       );
-          //     }),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                selected = !selected;
-                if (selected) {
-                  _scaleRadio = 0.8;
-                } else {
-                  _scaleRadio = 0.3;
-                }
-              });
-            },
-            child: Text(
-              'Animate',
-              style: TextStyle(fontSize: 15, color: Colors.red),
-            ),
-          )
         ],
       ),
     );
