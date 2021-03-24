e85ff68d78ac0a272ba8870235427021c8a1ed43
AriZhong <rest950@gmail.com>
2021-03-24 15:51:29 +0800

    20210324

--- lib/pages/individualphoto.dart
+++ lib/pages/individualphoto.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/model/photo.dart';
 import 'package:flutter_app/service/userrepository.dart';
 import 'package:http/http.dart' as http;
+import 'package:local_auth/local_auth.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:share/share.dart';
@@ -13,7 +14,12 @@ class IndividualPhotoPage extends StatefulWidget {
   _IndividualPhotoPageState createState() => _IndividualPhotoPageState();
 }
 
-class _IndividualPhotoPageState extends State<IndividualPhotoPage> {
+class _IndividualPhotoPageState extends State<IndividualPhotoPage>
+    with SingleTickerProviderStateMixin {
+  final auth = LocalAuthentication();
+  double _scaleRadio = 0.8;
+  bool selected = false;
+
   @override
   Widget build(BuildContext context) {
     //TODO: 取得Photo Data Model
@@ -26,7 +32,18 @@ class _IndividualPhotoPageState extends State<IndividualPhotoPage> {
         actions: [
           IconButton(
             icon: Icon(Icons.share),
-            onPressed: () async => await shareImage(photo),
+            onPressed: () async {
+              //TODO: 使用生物辨識
+              try {
+                if (await auth.canCheckBiometrics) {
+                  if (await auth.authenticate(
+                      localizedReason: 'Please authenticate to share photo.',
+                      biometricOnly: false)) {
+                    await shareImage(photo);
+                  }
+                }
+              } catch (e) {}
+            },
           )
         ],
       ),
@@ -37,12 +54,63 @@ class _IndividualPhotoPageState extends State<IndividualPhotoPage> {
             title: Text('Title: ${photo.title}'),
             leading: Text('Id: ${photo.id}'),
           ),
-          ClipRRect(
+          //TODO: AnimatedOpacity, AnimatedPadding, AnimatedContainer
+          AnimatedOpacity(
+            opacity: selected ? 0 : 1,
+            duration: Duration(milliseconds: 1500),
+            curve: Curves.linear,
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(15),
               child: Image.network(
                 photo.url,
-                width: width * 0.8,
-              )),
+              ),
+            ),
+          ),
+          // //TODO: AnimatedSwitcher
+          // AnimatedSwitcher(
+          //   duration: Duration(milliseconds: 1000),
+          //   child: selected
+          //       ? Container(
+          //           key: ValueKey(1),
+          //           color: Colors.white,
+          //           height: 100,
+          //           width: 50,
+          //         )
+          //       : FlutterLogo(
+          //           size: 20,
+          //         ),
+          //   switchInCurve: Curves.easeIn,
+          //   switchOutCurve: Curves.easeIn,
+          // ),
+          // //TODO: TweenAnimationBuilder
+          // TweenAnimationBuilder<double>(
+          //     tween: Tween<double>(begin: 0, end: 2 * math.pi),
+          //     duration: Duration(seconds: 2),
+          //     curve: Curves.easeIn,
+          //     builder: (__, angle, child) {
+          //       return Transform.rotate(
+          //         angle: angle,
+          //         child: FlutterLogo(
+          //           size: 100,
+          //         ),
+          //       );
+          //     }),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                selected = !selected;
+                if (selected) {
+                  _scaleRadio = 0.8;
+                } else {
+                  _scaleRadio = 0.3;
+                }
+              });
+            },
+            child: Text(
+              'Animate',
+              style: TextStyle(fontSize: 15, color: Colors.red),
+            ),
+          )
         ],
       ),
     );
